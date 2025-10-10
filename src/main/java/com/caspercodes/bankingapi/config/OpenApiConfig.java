ffdc78942e638a565833f3b0c1a3ee71c92e1480
Oluwasanumi <oluwasanumibalogun@gmail.com>
2025-10-10 14:20:11 +0100

    OpenAPI (Swagger)

--- src/main/java/com/caspercodes/bankingapi/config/OpenApiConfig.java
+++ src/main/java/com/caspercodes/bankingapi/config/OpenApiConfig.java
@@ -1,47 +1,50 @@
 package com.caspercodes.bankingapi.config;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(
+                title = "Banking API",
+                version = "1.0.0",
+                description = """
+                        A secure banking API that provides functionality for:
+                        - User authentication and authorization using JWT.
+                        - Account management
+                        - Transaction processing
+                        - Balance inquiries
+                        - Fund transfers
+                        
+                        All endpoints are secured (except auth) and require a valid JWT token for access.
+                        """,
+                contact = @io.swagger.v3.oas.annotations.info.Contact(
+                        name = "Oluwasanumi Balogun",
+                        email = "oluwasanumibalogun@gmail.com"
+                ),
+                license = @io.swagger.v3.oas.annotations.info.License(
+                        name = "MIT License",
+                        url = "https://opensource.org/license/mit/"
+                )),
+        servers = {
+                @Server(url = "http://localhost:8080", description = "Local server")
+        },
+        security = {
+                @io.swagger.v3.oas.annotations.security.SecurityRequirement(name = "bearerAuth")
+        }
+)
+@SecuritySchemes({
+        @io.swagger.v3.oas.annotations.security.SecurityScheme(
+                name = "bearerAuth",
+                type = io.swagger.v3.oas.annotations.enums.SecuritySchemeType.HTTP,
+                scheme = "bearer",
+                bearerFormat = "JWT",
+                description = "Enter JWT token"
+        )
+}
+)
 public class OpenApiConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        final String securitySchemeName = "bearerAuth";
-
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Banking API")
-                        .description("Secure Banking API with JWT Authentication")
-                        .version("1.0")
-                        .contact(new Contact()
-                                .name("Oluwasanumi Balogun")
-                                .email("oluwasanumibalogun@gmail.com")
-                        )
-                        .license(new License()
-                                .name("MIT License")
-                                .url("https://opensource.org/license/mit/"
-                        )
-                ))
-                .addSecurityItem(new SecurityRequirement()
-                        .addList(securitySchemeName)
-                )
-                .components(new Components()
-                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
-                                .name(securitySchemeName)
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")
-                                .description("Enter JWT Token")
-                        )
-                );
-    }
 }
